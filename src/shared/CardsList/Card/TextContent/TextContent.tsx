5709efdce688377157d4d0c956ccc4a8ca6bd8d8
Vladislav Surin <vlad_surin@mail.ru>
2023-05-23 19:33:28 +0300

    Refactored a Card component

--- src/shared/CardsList/Card/TextContent/TextContent.tsx
+++ src/shared/CardsList/Card/TextContent/TextContent.tsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { UserLink } from "./UserLink";
 import styles from './textContent.css';
-import { Title } from "./Title";
 
 export function TextContent() {
   return (
     <div className={styles.textContent}>
       <div className={styles.metaData}>
-        <UserLink />
-        <span className={styles.createdAt}>4 часа назад</span>
+        <div className={styles.userLink}>
+          <img
+            className={styles.avatar}
+            src="https://cdn.dribbble.com/userupload/3977833/file/original-eae541dd78f455f106443868aa7e3b7f.png?compress=1&resize=400x300&vertical=top"
+            alt="avatar"
+          />
+          <a className={styles.userName} href="#user-url">Дмитрий Гришин</a>
+        </div>
+        <span className={styles.createdAt}>
+          <span className={styles.publishedLabel}>опубликовано </span>
+          4 часа назад
+        </span>
       </div>
-      <Title />
+      <h2 className={styles.title}>
+        <a className={styles.postLink} href="#post-url">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, animi.
+        </a>
+      </h2>
     </div>
   );
 }
